f413822393835b0e88a8a028a5713ba34f7a141f
Sarah Yu <sarahyu0692@gmail.com>
2018-01-07 19:29:57 -0500

    fix route for deleting images from boards

--- controllers/boardsController.js
+++ controllers/boardsController.js
@@ -4,6 +4,7 @@ const Schema = require('../db/schema')
 const router = express.Router()
 
 const Board = Schema.Board
+const Image = Schema.Image
 
 router.get('/', (req, res) => {
 	Board.find()
@@ -29,9 +30,26 @@ router.put('/:id', (req, res) => {
 		.catch(err => console.log(err))
 })
 
+// delete route for boards
 router.delete('/:id', (req, res) => {
 	Board.findByIdAndRemove(req.params.id)
 		.then(board => res.json(board))
+		.catch(err => console.log(err))
+})
+
+// delete route for board images
+router.delete('/:board_id/images/:image_id', (req, res) => {
+	Board.findById(req.params.board_id)
+		.then(board => {
+			let imageIndex = board.images.findIndex(image => {
+				return image._id == req.params.image_id
+			})
+			board.images.splice(imageIndex, 1)
+			board
+				.save()
+				.then(board => res.json(board))
+				.catch(err => console.log(err))
+		})
 		.catch(err => console.log(err))
 })
 
