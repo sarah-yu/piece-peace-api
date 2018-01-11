c147eabdb4c05a01e521ec5dec1edbead9222685
Sarah Yu <sarahyu0692@gmail.com>
2018-01-11 16:02:19 -0500

    set up for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-// if (process.env.NODE_ENV == 'production') {
-// 	mongoose.connect(process.env.MLAB_URL)
-// } else {
-mongoose.connect('mongodb://localhost/piece-peace')
-// }
+if (process.env.NODE_ENV == 'production') {
+	mongoose.connect(process.env.MLAB_URL)
+} else {
+	mongoose.connect('mongodb://localhost/piece-peace')
+}
 
 mongoose.Promise = Promise
 
--- index.js
+++ index.js
@@ -27,6 +27,69 @@ app.get('/', (req, res) => {
 	res.send('nothing to see here')
 })
 
+app.post('/api/upload', (req, res) => {
+	// console.log('your file was submitted')
+	// console.log(req.body)
+
+	let myform = new formidable.IncomingForm()
+	myform.encoding = 'utf-8'
+	// myform.uploadDir = 'C:/Users/sarah/wdi/projects/piece-peace-api/uploads/'
+	myform.keepExtensions = true
+	myform.type = 'multipart'
+
+	console.log(Date)
+	console.log('what is going here')
+
+	console.log(myform.maxFieldsSize)
+
+	console.log(__dirname)
+
+	//
+	// console.log('is this working')
+	// console.log(form)
+	// every time a file has been uploaded successfully,
+	// rename it to it's orignal name
+	myform.on('file', function(field, file) {
+		fs.rename(file.path, path.join(__dirname, file.name))
+	})
+
+	// log any errors that occur
+	myform.on('error', function(err) {
+		console.log('An error has occured: \n' + err)
+	})
+
+	// once all the files have been uploaded, send a response to the client
+	myform.on('end', function() {
+		res.end('success')
+	})
+
+	// parse the incoming request containing the form data
+	myform.parse(req)
+
+	// myform.parse(req, function(err, field, file) {
+	// 	console.log('inside parse()')
+	//
+	// 	console.log(myform.bytesReceived)
+
+	// if (err) throw err
+
+	// console.log(`fields: ${fields}`)
+	// console.log(`files: ${files}`)
+	//
+	// var oldpath = files.image.path
+	// console.log(`oldpath: ${oldpath}`)
+	// var newpath =
+	// 	'C:/Users/sarah/wdi/projects/piece-peace-api' + files.image.name
+	//
+	// console.log(`newpath: ${newpath}`)
+	// fs.rename(oldpath, newpath, function(err) {
+	// 	if (err) console.log(err)
+	// 	res.write('File uploaded and moved!')
+	// 	res.end()
+	// })
+	// })
+})
+
 app.get('/api/users', (req, res) => {
 	User.find()
 		.then(users => res.json(users))
@@ -101,6 +164,6 @@ app.post('/api/register', function(req, res) {
 app.use('/api/images', imagesController)
 app.use('/api/boards', boardsController)
 
-app.listen(3001, function() {
-	console.log('express server up and running!')
+app.listen(process.env.PORT || 3001, function() {
+	console.log('express server is up and running!')
 })
